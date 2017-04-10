19a4c58063cd360bb49633f4a1814a8287a1aaed
sl1msn1per <sl1msn1per@live.com>
2017-04-10 09:10:23 +0000

    updated

--- mosaicer.py
+++ mosaicer.py
@@ -2,33 +2,37 @@ from PIL import Image
 import sys, getopt
 from io import StringIO, BytesIO
 
+def printusage():
+    print("""mosaicer.py -i <inputfile> -o <outputfile>
+    STDIN and STDOUT input/output can be specified by "-" where appropriate (minus quotes). STDOUT output will be in PNG format.
+    """, file=sys.stderr)
+
 inputfile = ''
 outputfile = ''
 try:
     opts, args = getopt.getopt(sys.argv[1:],"hi:o:")
 except getopt.GetoptError:
-    print('mosaicer.py -i <inputfile> -o <outputfile>', file=sys.stderr)
+    printusage()
     sys.exit(2)
 for opt, arg in opts:
     if opt == '-h':
-        print('mosaicer.py -i <inputfile> -o <outputfile>', file=sys.stderr)
+        printusage()
         sys.exit()
     elif opt == '-i':
         inputfile = arg
     elif opt == '-o':
         outputfile = arg
 if inputfile == '':
-    print('mosaicer.py -i <inputfile> -o <outputfile>', file=sys.stderr)
+    printusage()
     sys.exit(2)
 if outputfile == '':
-    print('mosaicer.py -i <inputfile> -o <outputfile>', file=sys.stderr)
+    printusage()
     sys.exit(2)
 
 if inputfile == '-':
     inputimage = Image.open(sys.stdin.buffer)
 else:
     inputimage = Image.open(inputfile)
-#print(inputimage.format, inputimage.size, inputimage.mode, file=sys.stderr)
 
 outputimage = Image.new(inputimage.mode,inputimage.size,0)
 pixelsin = inputimage.load()
