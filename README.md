fa0ffa8bd7d5ca39126a46fa17add7d0de1bcff0
sl1msn1per <sl1msn1per@live.com>
2017-04-11 22:24:10 +0000

    Updated readme and const interpolations

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Mosaicer
-A program to generate a psuedo mosaiced image for processing in a demosaicing algorithm.
+A set of programs for generating mosaiced images, and demosaicing them back to full RGB images.
 
-Python version require PIL (Python Imaging Library)
+Requires PIL (Python Imaging Library). Requires Python 3 (for print statements).
+
+Run -h on any of the scripts to get usage information. Input and output of almost all image types are supported (though lossless formats are the only ones that make sense in this context). Here's an example of how to use the stdout/stdin redirect to skip an intermediary mosaiced image save and load:
+
+./mosaicer.py -i in.png -o - | ./const_hue_norm.py -i - -o out.png
+
+Infact you can input a normal image into any of the interpolation scripts to get the same result as the corresponding mosaiced image; mosaicing first is not neccesary. Running a mosaiced image through will merely prove that I didn't cheat.
---
+++ const_hue.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+from PIL import Image
+import sys, getopt
+from io import StringIO, BytesIO
+
+def printusage():
+    print("""bilinear.py -i <inputfile> -o <outputfile>
+    STDIN and STDOUT input/output can be specified by "-" where appropriate (minus quotes). STDOUT output will be in PNG format.
+    """, file=sys.stderr)
+
+inputfile = ''
+outputfile = ''
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"hi:o:")
+except getopt.GetoptError:
+    printusage()
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == '-h':
+        printusage()
+        sys.exit()
+    elif opt == '-i':
+        inputfile = arg
+    elif opt == '-o':
+        outputfile = arg
+if inputfile == '':
+    printusage()
+    sys.exit(2)
+if outputfile == '':
+    printusage()
+    sys.exit(2)
+
+if inputfile == '-':
+    inputimage = Image.open(sys.stdin.buffer)
+else:
+    inputimage = Image.open(inputfile)
+
+outputimage = Image.new(inputimage.mode,inputimage.size,0)
+pixelsin = inputimage.load()
+pixelsout = outputimage.load()
+# Green first.
+for x in range(inputimage.width):
+    for y in range(inputimage.height):
+        if (x%2==1)&(y%2==0):
+            #At red pixel
+            gtot=0
+            gcount=0
+            if (y-1 > 0):
+                gcount += 1
+                gtot += pixelsin[x,y-1][1]
+            if (y+1 < inputimage.height):
+                gcount += 1
+                gtot += pixelsin[x,y+1][1]
+            if (x-1 > 0):
+                gcount += 1
+                gtot += pixelsin[x-1,y][1]
+            if (x+1 < inputimage.width):
+                gcount += 1
+                gtot += pixelsin[x+1,y][1]
+            pixelsin[x,y] = (pixelsin[x,y][0],int(gtot/gcount),pixelsin[x,y][2])
+        elif (x%2==0)&(y%2==1):
+            # At blue pixel
+            gtot=0
+            gcount=0
+            if (y-1 > 0):
+                gcount += 1
+                gtot += pixelsin[x,y-1][1]
+            if (y+1 < inputimage.height):
+                gcount += 1
+                gtot += pixelsin[x,y+1][1]
+            if (x-1 > 0):
+                gcount += 1
+                gtot += pixelsin[x-1,y][1]
+            if (x+1 < inputimage.width):
+                gcount += 1
+                gtot += pixelsin[x+1,y][1]
+            pixelsin[x,y] = (pixelsin[x,y][0],int(gtot/gcount),pixelsin[x,y][2])
+# Now find red/blue
+for x in range(inputimage.width):
+    for y in range(inputimage.height):
+        if (x%2==1)&(y%2==0):
+            #At red pixel, need blue
+            btot=0
+            bcount=0
+            if (y-1 > 0)&(x-1 > 0):
+                bcount += 1
+                btot += pixelsin[x-1,y-1][2]/(pixelsin[x-1,y-1][1] + 1)
+            if (y-1 > 0)&(x+1 < inputimage.width):
+                bcount += 1
+                btot += pixelsin[x+1,y-1][2]/(pixelsin[x+1,y-1][1] + 1)
+            if (y+1 < inputimage.height)&(x+1 < inputimage.width):
+                bcount += 1
+                btot += pixelsin[x+1,y+1][2]/(pixelsin[x+1,y+1][1] + 1)
+            if (y+1 < inputimage.height)&(x-1 > 0):
+                bcount += 1
+                btot += pixelsin[x-1,y+1][2]/(pixelsin[x-1,y+1][1] + 1)
+            pixelsout[x,y] = (pixelsin[x,y][0],pixelsin[x,y][1],int((pixelsin[x,y][1]+1)*(btot/bcount)))
+        elif (x%2==0)&(y%2==1):
+            # At blue pixel, need red
+            rtot=0
+            rcount=0
+            if (y-1 > 0)&(x-1 > 0):
+                rcount += 1
+                rtot += pixelsin[x-1,y-1][0]/(pixelsin[x-1,y-1][1] + 1)
+            if (y-1 > 0)&(x+1 < inputimage.width):
+                rcount += 1
+                rtot += pixelsin[x+1,y-1][0]/(pixelsin[x+1,y-1][1] + 1)
+            if (y+1 < inputimage.height)&(x+1 < inputimage.width):
+                rcount += 1
+                rtot += pixelsin[x+1,y+1][0]/(pixelsin[x+1,y+1][1] + 1)
+            if (y+1 < inputimage.height)&(x-1 > 0):
+                rcount += 1
+                rtot += pixelsin[x-1,y+1][0]/(pixelsin[x-1,y+1][1] + 1)
+            pixelsout[x,y] = (int((pixelsin[x,y][1]+1)*(rtot/rcount)),pixelsin[x,y][1],pixelsin[x,y][2])
+        elif (x%2==0)&(y%2==0):
+            #At green pixel, top-left. Need red and blue.
+            btot=0
+            bcount=0
+            if (y-1 > 0):
+                bcount += 1
+                btot += pixelsin[x,y-1][2]/(pixelsin[x,y-1][1] + 1)
+            if (y+1 < inputimage.height):
+                bcount += 1
+                btot += pixelsin[x,y+1][2]/(pixelsin[x,y+1][1] + 1)
+            rtot=0
+            rcount=0
+            if (x-1 > 0):
+                rcount += 1
+                rtot += pixelsin[x-1,y][0]/(pixelsin[x-1,y][1] + 1)
+            if (x+1 < inputimage.width):
+                rcount += 1
+                rtot += pixelsin[x+1,y][0]/(pixelsin[x+1,y][1] + 1)
+            pixelsout[x,y] = (int((pixelsin[x,y][1]+1)*(rtot/rcount)),pixelsin[x,y][1],int((pixelsin[x,y][1]+1)*(btot/bcount)))
+        else:
+            #At green pixel, bottom-right
+            rtot=0
+            rcount=0
+            if (y-1 > 0):
+                rcount += 1
+                rtot += pixelsin[x,y-1][0]/(pixelsin[x,y-1][1] + 1)
+            if (y+1 < inputimage.height):
+                rcount += 1
+                rtot += pixelsin[x,y+1][0]/(pixelsin[x,y+1][1] + 1)
+            btot=0
+            bcount=0
+            if (x-1 > 0):
+                bcount += 1
+                btot += pixelsin[x-1,y][2]/(pixelsin[x-1,y][1] + 1)
+            if (x+1 < inputimage.width):
+                bcount += 1
+                btot += pixelsin[x+1,y][2]/(pixelsin[x+1,y][1] + 1)
+            pixelsout[x,y] = (int((pixelsin[x,y][1]+1)*(rtot/rcount)),pixelsin[x,y][1],int((pixelsin[x,y][1]+1)*(btot/bcount)))
+
+if outputfile == '-':
+    out = BytesIO()
+    outputimage.save(out,inputimage.format)
+    sys.stdout.buffer.write(out.getvalue())
+else:
+    outputimage.save(outputfile)
